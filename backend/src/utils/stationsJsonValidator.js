03481d5711e115711ea667661b6f869967151efb
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-12-18 20:49:23 +0200

    added helper function to validate stations

---
+++ backend/src/utils/stationsJsonValidator.js
@@ -0,0 +1,32 @@
+const mockStationsImport = require("../../MockValues/mockStationsUnvalidated");
+const mockStations = mockStationsImport['getMockValue'];
+
+
+/**
+ *
+ * @param stationsJson{object[]}
+ * @returns {*object[]}
+ */
+const stationsJsonValidator = (stationsJson) => {
+    // console.log(mockStations);
+    let result = [];
+
+    // mockStations.map(s=>{
+    stationsJson.map(s=>{
+        let updatedValues = {};
+        for (let [key, value] of Object.entries(s)){
+            if(key.includes('Adress')){
+                key = 'Address';
+            }
+
+            updatedValues[key] = value;
+            // console.log(updatedValues);
+        }
+        result.push(updatedValues);
+    })
+    // console.log(result)
+    return result;
+}
+
+
+module.exports.stationsJsonValidator = stationsJsonValidator;
