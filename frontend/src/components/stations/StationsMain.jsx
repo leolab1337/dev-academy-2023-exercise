120569927161ff6a3ec16cfffea4346e238b3542
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-12-19 09:55:40 +0200

    added pages

---
+++ frontend/src/components/stations/StationsMain.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StationsMain = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default StationsMain;
---
+++ frontend/src/pages/StationsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Container} from "react-bootstrap";
+
+const StationsPage = () => {
+    return (
+        <Container>
+            it is stations page
+        </Container>
+    );
+};
+
+export default StationsPage;
