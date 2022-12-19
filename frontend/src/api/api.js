ee14bd1381218d70ebf38e656e7f3ecb9117613a
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-12-19 23:44:58 +0200

    added api functions

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -13,7 +13,83 @@ export const fetchAllData = async (url) => {
         credentials: 'include'
     }
 
+    try{
     const resp = await fetch(url, reqOptions);
-    const respJson = resp.json();
-    return respJson
+        return resp.json();
+    }
+    catch (e){
+        return e;
+    }
+
+}
+
+/**
+ *
+ * @param url{string}
+ * @returns {Promise<any>}
+ */
+export const fetchOneById = async (url) => {
+    const reqOptions = {
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        method: 'GET',
+        credentials: 'include'
+    }
+    try{
+        const resp = await fetch(url, reqOptions);
+        return resp.json();
+    }
+    catch (e){
+        return e;
+    }
+}
+
+/**
+ *
+ * @param url{string}
+ * @returns {Promise<any>}
+ */
+export const deleteById = async (url) => {
+    const reqOptions = {
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        method: 'DELETE',
+        credentials: 'include'
+    }
+    try{
+        const resp = await fetch(url, reqOptions);
+        return resp.json();
+    }
+    catch (e){
+        return e;
+    }
+}
+
+/**
+ *
+ * @param url{string}
+ * @param reqData{object}
+ * @returns {Promise<any>}
+ */
+export const postNewOne = async (url,reqData) => {
+    const reqOptions = {
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        credentials: 'include',
+        // body: {...reqData}
+        body: JSON.stringify({...reqData})
+    }
+    try{
+        const resp = await fetch(url, reqOptions);
+        console.log(reqOptions);
+        console.log(resp);
+        return resp.json();
+    }
+    catch (e){
+        return e;
+    }
 }
