236ecd4bf9bd6047a06e736327c1892937874d15
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-12-19 09:58:19 +0200

    added setup files

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import RouterComponent from "./components/RouterComponent";
+
+function App() {
+
+  return (
+      <div className="App" id="container">
+
+        <RouterComponent/>
+
+      </div>
+  );
+}
+
+export default App;
