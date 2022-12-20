aa5980e51e1fd6357c8070658a3e3ae575ccb731
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-12-20 17:24:19 +0200

    added map view for one station page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
+import "leaflet/dist/leaflet.css";
 import './App.css'
 import RouterComponent from "./components/RouterComponent";
+
 
 function App() {
 
   return (
       <div className="App" id="container">
-
         <RouterComponent/>
-
       </div>
   );
 }
---
+++ frontend/src/components/map/Map.jsx
@@ -0,0 +1,44 @@
+import "leaflet/dist/leaflet.css";
+// import styles from './map.module.css'
+import {MapContainer, Marker, CircleMarker, TileLayer} from "react-leaflet";
+
+
+const svgMarker = {
+    path:
+        "M10.453 14.016l6.563-6.609-1.406-1.406-5.156 5.203-2.063-2.109-1.406 1.406zM12 2.016q2.906 0 4.945 2.039t2.039 4.945q0 1.453-0.727 3.328t-1.758 3.516-2.039 3.070-1.711 2.273l-0.75 0.797q-0.281-0.328-0.75-0.867t-1.688-2.156-2.133-3.141-1.664-3.445-0.75-3.375q0-2.906 2.039-4.945t4.945-2.039z",
+    fillColor: "blue",
+    fillOpacity: 0.6,
+    strokeWeight: 0,
+    rotation: 0,
+    scale: 2
+    // anchor: new google.maps.Point(15, 30),
+};
+
+
+const Map = ({x= 24.9442,y= 60.166}) => {
+
+    const position = [y, x];
+
+    console.log(position);
+    return (
+        <div>
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false} style={{width: '35vw' ,height : '315px' , minWidth:'180px'}}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                    <CircleMarker
+                        center={position}
+                        color={"#000000"}
+                        fillColor={"red"}
+                        stroke={true}
+                        fillOpacity={true}
+                        weight={1}
+                        icon={svgMarker}
+                    />
+            </MapContainer>
+        </div>
+    );
+};
+
+export default Map;
