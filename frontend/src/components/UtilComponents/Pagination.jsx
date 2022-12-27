5404828916b65f2f7560d0531f247317833fe5de
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-12-27 19:32:01 +0200

    Added input validation and error message for page size in pagination component

--- frontend/src/components/UtilComponents/Pagination.jsx
+++ frontend/src/components/UtilComponents/Pagination.jsx
@@ -25,7 +25,9 @@ export const OwnPagination = ({pageSize,pageNumber,totalPages,handlePrevPage,han
             </div>
             <div>
                 Results per page:{' '}
-                <input type="number" value={pageSize} onChange={handlePageSizeChange}  min="1" max="100"/>
+
+                <input id='numberInput' type="number" value={pageSize} onChange={handlePageSizeChange}  min="1" max="100"/>
+                <label style={{fontSize: '0.8rem', marginLeft: '0.4rem', color: '#bd111f'}} htmlFor="numberInput">Values between(1-100)</label>
             </div>
             <span style={{color: 'red'}}>{error && error}</span>
         </div>
