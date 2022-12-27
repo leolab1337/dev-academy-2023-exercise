50528f7d59dad28691fcef900e411d4f821d0eaf
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-12-27 19:23:56 +0200

    Add One Journey functional components for displaying information about a single journey

---
+++ frontend/src/components/journeys/OneJourney.jsx
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from 'react';
+import {useNavigate, useParams} from "react-router";
+import {fetchOneById} from "../../api/api";
+import {v4 as uuid} from "uuid";
+import {changeStringDateToDate} from "../../utils/changeStringDateToDate";
+
+/**
+ * OneJourney is a functional component that displays information about a single journey.
+ * @return {JSX.Element} - A JSX element representing the UI for the OneJourney component.
+ */
+const OneJourney = () => {
+
+    const navigate = useNavigate();
+    const params = useParams();
+    const journeyID = params.id;
+
+    const [fetchError,setFetchError] = useState(null);
+    let [journeyObject,setJourneyObject] = useState({});
+
+    useEffect( ()=>{
+        fetchOneById(`${process.env.REACT_APP_SERVER_URL}/journeys/${journeyID}`).then(r=>{
+            if(r.isSuccess === true){
+                setJourneyObject(changeStringDateToDate(r.result[0]));
+                setFetchError(null);
+            }
+            else{
+                navigate('/404');
+            }
+        }).catch(e => setFetchError(e));
+
+
+    },[navigate,journeyID]);
+
+    // const goToStation = stationID => navigate(`${process.env.REACT_APP_SERVER_URL}/stations/${stationID}`);
+    const goToStation = stationID => navigate(`/stations/${stationID}`);
+
+    return (
+        <>
+            <div >
+                <ul className=''>
+                    {journeyObject && Object.entries(journeyObject).map(([key,val]) => (
+                        <li key={uuid()}>
+
+                            {key}: {val}
+                            {key === 'Departure_station_id' || key === 'Return_station_id'
+
+                                ?
+                                <button onClick={()=>goToStation(val)} style={{marginLeft: '1rem',backgroundColor  : '#36d25a'}}>Take a look</button>
+                                : ''
+                            }
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <br/> <b>{fetchError && fetchError.toString() + '; Possibly problems with backend connection'}</b>
+        </>
+    );
+};
+
+export default OneJourney;
---
+++ frontend/src/pages/OneJourneyPage.jsx
@@ -0,0 +1,21 @@
+import {Container} from "react-bootstrap";
+import GoBackButton from "../components/GoBackButton";
+import OneJourney from "../components/journeys/OneJourney";
+
+/**
+ * A component that displays a page with information about a single journey.
+ * @return {JSX.Element} The `Container` element with the page content, including the `GoBackButton` and `OneStation` components.
+ */
+const OneJourneyPage = () => {
+    return (
+        <Container>
+            <GoBackButton/>
+            <h1 className='d-flex justify-content-center'>Journey info</h1>
+            <div className='d-flex justify-content-center'>
+                <OneJourney/>
+            </div>
+        </Container>
+    );
+};
+
+export default OneJourneyPage;
