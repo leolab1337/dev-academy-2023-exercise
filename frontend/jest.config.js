a5e97bcee062c939d252227255a47942569569d6
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-05-19 14:56:05 +0300

    add test configuration , jest&storybook

---
+++ frontend/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // other Jest config options
+    setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+    // other Jest config options
+};
---
+++ frontend/mocks/reactLeafletMock.js
@@ -0,0 +1,4 @@
+//without it, the test library has a conflict with react-leaflet
+module.exports = {
+    useMapEvents: () => {}
+}
