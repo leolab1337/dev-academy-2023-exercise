a56ab9d32e511d9af3d364818ac95a3207282ced
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2023-05-20 17:18:45 +0300

    refactor pageloader; change location

--- frontend/src/components/widgets/PageLoader/index.jsx
+++ frontend/src/components/widgets/PageLoader/index.jsx
@@ -1,5 +1,5 @@
 import styles from "./PageLoader.module.css"
-import {Loader} from "../Loader";
+import {Loader} from "../../utilComponents/Loader";
 
 export const PageLoader = ({ className = '' }) => (
     <div className={styles.PageLoader + ' ' + className}>
